0c07dcd39b1d95b0979c04266aafc585dc1c8aa5
Brandon <brandon.a.pinkerton@gmail.com>
2021-10-30 18:50:54 -0500

    init commit

---
+++ src/main/java/com/titan/tech/Satisfactory/SatisfactoryApplication.java
@@ -0,0 +1,13 @@
+package com.titan.tech.Satisfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatisfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatisfactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/titan/tech/Satisfactory/SatisfactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titan.tech.Satisfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatisfactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

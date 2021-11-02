8857a6eaf4f8e5d9bc22683d1588b7242a1e0f7e
Brandon <brandon.a.pinkerton@gmail.com>
2021-11-02 09:44:32 -0500

    refactor RecipeItem entity to manually implement @Data

--- src/main/java/com/tech/titan/satisfactory/api/model/RecipeItem.java
+++ src/main/java/com/tech/titan/satisfactory/api/model/RecipeItem.java
@@ -1,20 +1,14 @@
 package com.tech.titan.satisfactory.api.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
-@EqualsAndHashCode(callSuper = true)
+
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "recipe_items")
 public class RecipeItem extends RepresentationModel<RecipeItem> implements Serializable {
 
@@ -30,10 +24,70 @@ public class RecipeItem extends RepresentationModel<RecipeItem> implements Seria
 
     private double quantity;
 
+    public RecipeItem(){
+
+    }
+
     public RecipeItem(Item item, Recipe recipe, double quantity){
         this.recipeItemId = new RecipeItemId(recipe.getRecipeId(),item.getItemId());
         this.item = item;
         this.recipe = recipe;
         this.quantity = quantity;
+    }
+
+    public RecipeItemId getRecipeItemId() {
+        return recipeItemId;
+    }
+
+    public void setRecipeItemId(RecipeItemId recipeItemId) {
+        this.recipeItemId = recipeItemId;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeItem{" +
+                "recipeItemId=" + recipeItemId +
+                ", item=" + item +
+                ", recipe=" + recipe +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        RecipeItem that = (RecipeItem) o;
+        return Double.compare(that.quantity, quantity) == 0 && Objects.equals(recipeItemId, that.recipeItemId) && Objects.equals(item, that.item) && Objects.equals(recipe, that.recipe);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), recipeItemId, item, recipe, quantity);
     }
 }
